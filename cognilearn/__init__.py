ecc878df97c9490fbdd87ea309cc48e5aca7a821
Aditya Jain <adityajn105@gmail.com>
2019-03-30 15:07:04 +0530

    init created

---
+++ cognilearn/__init__.py
@@ -0,0 +1,11 @@
+from . import metrics
+from . import feature_selection
+from . import analysis
+from . import ensemble
+from . import preprocessing
+
+
+__version__ = '0.0.3'
+__author__ = 'Aditya Jain'
+__email__ = 'aditya.jain@cognizant.com'
+__modules__ = ['metrics','feature_selection','analysis','ensemble','preprocessing']
