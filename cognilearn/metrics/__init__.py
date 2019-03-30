bb3b766b15caeb17c11925ed9f164c10ea0f33e5
Aditya Jain <adityajn105@gmail.com>
2019-03-30 15:09:32 +0530

    metrics

---
+++ cognilearn/metrics/__init__.py
@@ -0,0 +1,6 @@
+from .roc_curves import roc_curves 
+from .confusion_matrix_modified import confusion_matrix_modified
+from .sensitivity import sensitivity
+from .specificity import specificity
+from .accuracy import accuracy
+from .specificity_vs_sensitivity import specificity_vs_sensitivity
